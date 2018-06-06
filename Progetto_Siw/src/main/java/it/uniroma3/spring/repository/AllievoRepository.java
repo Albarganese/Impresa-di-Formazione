365fe903d3bc0b62fc558dacb00a4e210a20ff28
annaledaCoccia <39617638+annaledaCoccia@users.noreply.github.com>
2018-06-06 14:52:39 +0200

    Update AllievoRepository.java

--- Progetto_Siw/src/main/java/it/uniroma3/spring/repository/AllievoRepository.java
+++ Progetto_Siw/src/main/java/it/uniroma3/spring/repository/AllievoRepository.java
@@ -1,18 +1,20 @@
 package it.uniroma3.spring.repository;
 
+import java.sql.Date;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
 import it.uniroma3.spring.model.Allievo;
 
-public interface AllievoRepository extends CrudRepository<Allievo, Long> {
-
-	 List<Allievo> findByCognome(String lastName);
-
-	 List<Allievo> findByNome(String lastName);
-	 
-	 List<Allievo> findByNomeAndCognomeAndEmail(String nome, String cognome, String email);
-	 
+public interface AllievoRepository extends CrudRepository<Allievo, Long > {
 	    
+	public List<Allievo> findByNome(String nome);
+
+    public  List<Allievo> findByCognome(String cognome);
+    
+    public List<Allievo> findByNomeAndCognomeAndDataDiNascitaAndLuogoDiNascita(
+    		String nome, String Cognome, Date dataDiNascita, String luogoDiNascita);
+
+	public List<Allievo> findByLuogoDiNascita(String luogoDiNascita);
 }
